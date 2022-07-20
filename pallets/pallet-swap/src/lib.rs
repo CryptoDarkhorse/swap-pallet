72bacc26bd3bbc5d575e3aae34c6b9bc278325d1
Cameron Knight <c.knight8817@gmail.com>
2022-07-20 09:02:51 +0800

    version 0.0.1

--- pallets/pallet-swap/src/lib.rs
+++ pallets/pallet-swap/src/lib.rs
@@ -15,13 +15,18 @@ mod benchmarking;
 
 #[frame_support::pallet]
 pub mod pallet {
+    use core::convert::TryFrom;
+
+    use frame_support::dispatch::PostDispatchInfo;
     use frame_support::pallet_prelude::*;
     use frame_support::traits::{Currency, ExistenceRequirement};
     use frame_system::pallet_prelude::*;
+    use pallet_erc20::Pallet as ERC20;
     use sp_core::U256;
+
     /// Configure the pallet by specifying the parameters and types on which it depends.
     #[pallet::config]
-    pub trait Config: frame_system::Config {
+    pub trait Config: frame_system::Config + pallet_erc20::Config {
         /// Because this pallet emits events, it depends on the runtime's definition of an event.
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
 
@@ -35,14 +40,35 @@ pub mod pallet {
     #[pallet::generate_store(pub(super) trait Store)]
     pub struct Pallet<T>(_);
 
+    // owner of swap pool
+    #[pallet::storage]
+    #[pallet::getter(fn owner)]
+    pub type Owner<T: Config> = StorageValue<_, T::AccountId, ValueQuery>;
+
+    // orders information
     #[pallet::storage]
     #[pallet::getter(fn order_count)]
-    pub type OrderCount<T: Config> = StorageValue<_, U256, ValueQuery>;
+    pub type OrderCount<T: Config> = StorageValue<_, u64, ValueQuery>;
 
     #[pallet::storage]
-    #[pallet::getter(fn order_owners)]
-    pub type OrderOwners<T: Config> =
-        StorageMap<_, Blake2_128Concat, U256, T::AccountId, ValueQuery>;
+    #[pallet::getter(fn order_owner)]
+    pub type OrderOwner<T: Config> = StorageMap<_, Blake2_128Concat, u64, T::AccountId, ValueQuery>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn order_token_id)]
+    pub type OrderTokenId<T: Config> = StorageMap<_, Blake2_128Concat, u64, u64, ValueQuery>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn order_volume)]
+    pub type OrderVolume<T: Config> = StorageMap<_, Blake2_128Concat, u64, U256, ValueQuery>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn order_price)]
+    pub type OrderPrice<T: Config> = StorageMap<_, Blake2_128Concat, u64, U256, ValueQuery>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn order_status)]
+    pub type OrderStatus<T: Config> = StorageMap<_, Blake2_128Concat, u64, bool, ValueQuery>;
 
     // Pallets use events to inform users when important changes are made.
     // https://docs.substrate.io/v3/runtime/events-and-errors
@@ -50,53 +76,24 @@ pub mod pallet {
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
         // parameters: [token_id, amount, price, owner]
-        SellOrderCreated(u128, U256, U256, T::AccountId),
+        SellOrderCreated(u64, U256, U256, T::AccountId),
         // parameters: [order_id, status]
-        SellOrderCancelled(u128, bool),
+        SellOrderCancelled(u64, bool),
         // parameters: [order_id, amount of token sold, amount paid, seller, buyer]
-        BuyOrderFilled(u128, U256, U256, T::AccountId, T::AccountId),
+        BuyOrderFilled(u64, U256, U256, T::AccountId, T::AccountId),
     }
 
     // Errors inform users that something went wrong.
     #[pallet::error]
     pub enum Error<T> {
-        /// Error names should be descriptive.
-        NoneValue,
-        /// Errors should have helpful documentation associated with them.
-        StorageOverflow,
-
-        /// Deadline hit.
-        Deadline,
-        /// Zero tokens supplied.
-        ZeroTokens,
-        /// Zero reserve supplied.
-        ZeroAmount,
-        /// No Swap exists at this Id.
-        NoSwapExists,
-        /// A Swap already exists for a particular TokenId.
-        SwapAlreadyExists,
-        /// Requested zero liquidity.
-        RequestedZeroLiquidity,
-        /// Would add too many tokens to liquidity.
-        TooManyTokens,
-        /// Not enough liquidity created.
-        TooLowLiquidity,
-        /// No currency is being swapped.
-        NoCurrencySwapped,
-        /// No tokens are being swapped.
-        NoTokensSwapped,
-        /// Trying to burn zero shares.
-        BurnZeroShares,
-        /// No liquidity in the swap.
-        NoLiquidity,
-        /// Not enough currency will be returned.
-        NotEnoughCurrency,
-        /// Not enough tokens will be returned.
-        NotEnoughTokens,
-        /// Swap would cost too much in currency.
-        TooExpensiveCurrency,
-        /// Swap would cost too much in tokens.
-        TooExpensiveTokens,
+        Overflow,
+        InvalidOrderId,
+        AlreadyCancelled,
+        NotOwner,
+        CancelledOrFulfilled,
+        VolumeTooMuch,
+        BalanceError,
+        InsufficientCurrency,
     }
 
     #[pallet::hooks]
@@ -111,26 +108,45 @@ pub mod pallet {
         #[pallet::weight(10_000)]
         pub fn create_sell_order(
             origin: OriginFor<T>,
-            token_id: u128,
+            token_id: u64,
             volume: U256,
             price: U256,
-            currency_amount: BalanceOf<T>, // Amount of base currency to lock.
-        ) -> DispatchResult {
-            let who = ensure_signed(origin)?;
+        ) -> DispatchResultWithPostInfo {
+            let seller = ensure_signed(origin.clone())?;
 
-            // TODO: implement
-            T::Currency::transfer(&who, &who, currency_amount, ExistenceRequirement::KeepAlive)?;
+            // transfer token to owner
+            ERC20::<T>::transfer(origin, <Owner<T>>::get(), volume)?;
 
-            Self::deposit_event(Event::SellOrderCreated(token_id, volume, price, who));
-            Ok(())
+            let order_id = Self::order_count();
+
+            OrderCount::<T>::put(order_id + 1);
+
+            // order values
+            OrderOwner::<T>::insert(order_id, seller.clone());
+            OrderTokenId::<T>::insert(order_id, token_id);
+            OrderVolume::<T>::insert(order_id, volume);
+            OrderPrice::<T>::insert(order_id, price);
+            OrderStatus::<T>::insert(order_id, true);
+
+            Self::deposit_event(Event::SellOrderCreated(token_id, volume, price, seller));
+
+            Ok(PostDispatchInfo::from(Some(order_id)))
         }
 
         // Cancel
         #[pallet::weight(10_000)]
-        pub fn cancel_sell_order(origin: OriginFor<T>, order_id: u128) -> DispatchResult {
-            let _owner = ensure_signed(origin)?;
+        pub fn cancel_sell_order(origin: OriginFor<T>, order_id: u64) -> DispatchResult {
+            let seller = ensure_signed(origin.clone())?;
 
-            // TODO: implement
+            // check validity
+            ensure!(order_id <= Self::order_count(), Error::<T>::InvalidOrderId);
+            ensure!(seller == Self::order_owner(order_id), Error::<T>::NotOwner);
+            ensure!(Self::order_status(order_id), Error::<T>::AlreadyCancelled);
+
+            OrderStatus::<T>::insert(order_id, false);
+
+            // transfer token to owner
+            ERC20::<T>::transfer_from(origin, seller, Self::owner(), Self::order_volume(order_id))?;
 
             Self::deposit_event(Event::SellOrderCancelled(order_id, true));
             Ok(())
@@ -140,17 +156,78 @@ pub mod pallet {
         #[pallet::weight(10_000)]
         pub fn buy_order(
             origin: OriginFor<T>,
+            order_id: u64,
             _project_id: u32,
             _bundle_id: u32,
-            price: U256,
             volume: U256,
+            currency_amount: BalanceOf<T>,
         ) -> DispatchResult {
-            let buyer = ensure_signed(origin)?;
+            let buyer = ensure_signed(origin.clone())?;
 
-            // TODO: implement
+            // check validity of order
+            ensure!(order_id <= Self::order_count(), Error::<T>::InvalidOrderId);
+            ensure!(
+                Self::order_status(order_id),
+                Error::<T>::CancelledOrFulfilled
+            );
+            ensure!(
+                Self::order_volume(order_id) >= volume,
+                Error::<T>::VolumeTooMuch
+            );
 
-            let order_id: u128 = 0;
-            let seller: T::AccountId = buyer.clone();
+            // calculate necessary currency values
+            let price = Self::order_price(order_id);
+            let required_currency = BalanceOf::<T>::try_from((price * volume).as_u128())
+                .ok()
+                .ok_or(Error::<T>::BalanceError)?;
+
+            // calculate fee
+            let payment_fee =
+                required_currency * BalanceOf::<T>::from(2u32) / BalanceOf::<T>::from(100u32);
+            let purchase_fee = BalanceOf::<T>::try_from(volume.as_u128())
+                .ok()
+                .ok_or(Error::<T>::BalanceError)?
+                * BalanceOf::<T>::from(2u32)
+                / BalanceOf::<T>::from(10u32);
+
+            let total_fee = payment_fee + purchase_fee;
+
+            // ensure payment amount
+            ensure!(
+                currency_amount >= required_currency + total_fee,
+                Error::<T>::InsufficientCurrency
+            );
+
+            let seller = Self::order_owner(order_id);
+
+            // mark order as fulfilled
+            OrderStatus::<T>::insert(order_id, false);
+
+            // move funds
+
+            // move currency to seller
+            T::Currency::transfer(
+                &buyer,
+                &seller,
+                required_currency,
+                ExistenceRequirement::KeepAlive,
+            )?;
+
+            // move fee to owner
+            T::Currency::transfer(
+                &buyer,
+                &Self::owner(),
+                total_fee,
+                ExistenceRequirement::KeepAlive,
+            )?;
+
+            // move token to buyer
+            ERC20::<T>::transfer(origin.clone(), buyer.clone(), volume)?;
+            ERC20::<T>::transfer(
+                origin.clone(),
+                seller.clone(),
+                Self::order_volume(order_id) - volume,
+            )?;
 
             Self::deposit_event(Event::BuyOrderFilled(
                 order_id, volume, price, seller, buyer,
