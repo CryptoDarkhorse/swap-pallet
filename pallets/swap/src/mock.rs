d61bbd2d752c0bcd4d4d22496dad71767270fb55
CryptoDarkhorse <c.knight8817@gmail.com>
2022-11-27 00:16:05 +0800

    Mock config for swap-pallet

--- pallets/swap/src/mock.rs
+++ pallets/swap/src/mock.rs
@@ -1,7 +1,7 @@
-use crate as pallet_template;
-use frame_support::traits::{ConstU16, ConstU64};
-use frame_system as system;
-use sp_core::H256;
+use crate as pallet_swap;
+use frame_support::traits::ConstU64;
+
+use sp_core::{ConstU32, H256};
 use sp_runtime::{
     testing::Header,
     traits::{BlakeTwo256, IdentityLookup},
@@ -17,16 +17,17 @@ frame_support::construct_runtime!(
         NodeBlock = Block,
         UncheckedExtrinsic = UncheckedExtrinsic,
     {
-        System: frame_system,
-        TemplateModule: pallet_template,
+        System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+        Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
+        Assets: pallet_assets::{Pallet, Call, Storage, Event<T>},
+        TemplateModule: pallet_swap::{Pallet, Call, Storage, Event<T>},
     }
 );
 
-impl system::Config for Test {
+impl frame_system::Config for Test {
     type BaseCallFilter = frame_support::traits::Everything;
     type BlockWeights = ();
     type BlockLength = ();
-    type DbWeight = ();
     type RuntimeOrigin = RuntimeOrigin;
     type RuntimeCall = RuntimeCall;
     type Index = u64;
@@ -38,24 +39,56 @@ impl system::Config for Test {
     type Header = Header;
     type RuntimeEvent = RuntimeEvent;
     type BlockHashCount = ConstU64<250>;
+    type DbWeight = ();
     type Version = ();
     type PalletInfo = PalletInfo;
-    type AccountData = ();
+    type AccountData = pallet_balances::AccountData<u64>;
     type OnNewAccount = ();
     type OnKilledAccount = ();
     type SystemWeightInfo = ();
-    type SS58Prefix = ConstU16<42>;
+    type SS58Prefix = ();
     type OnSetCode = ();
-    type MaxConsumers = frame_support::traits::ConstU32<16>;
+    type MaxConsumers = ConstU32<2>;
 }
 
-impl pallet_template::Config for Test {
+impl pallet_balances::Config for Test {
+    type Balance = u64;
+    type DustRemoval = ();
     type RuntimeEvent = RuntimeEvent;
+    type ExistentialDeposit = ConstU64<1>;
+    type AccountStore = System;
+    type WeightInfo = ();
+    type MaxLocks = ();
+    type MaxReserves = ();
+    type ReserveIdentifier = [u8; 8];
+}
+
+impl pallet_assets::Config for Test {
+    type RuntimeEvent = RuntimeEvent;
+    type Balance = u64;
+    type AssetId = u32;
+    type Currency = Balances;
+    type ForceOrigin = frame_system::EnsureRoot<u64>;
+    type AssetDeposit = ConstU64<1>;
+    type AssetAccountDeposit = ConstU64<10>;
+    type MetadataDepositBase = ConstU64<1>;
+    type MetadataDepositPerByte = ConstU64<1>;
+    type ApprovalDeposit = ConstU64<1>;
+    type StringLimit = ConstU32<50>;
+    type Freezer = ();
+    type WeightInfo = ();
+    type Extra = ();
+}
+
+impl pallet_swap::Config for Test {
+    type RuntimeEvent = RuntimeEvent;
+    type CurrencyTokenId = ConstU32<0>;
+    type Owner = ConstU64<0>;
 }
 
 // Build genesis storage according to the mock runtime.
 pub fn new_test_ext() -> sp_io::TestExternalities {
-    system::GenesisConfig::default()
+    frame_system::GenesisConfig::default()
         .build_storage::<Test>()
         .unwrap()
         .into()
